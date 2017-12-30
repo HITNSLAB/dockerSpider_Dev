20725f9c953b66bd768353c43c6f96304b45fca4
YanzheL <lee.yanzhe@yanzhe.org>
2017-12-30 20:25:38 +0800

    nist intranet test env

--- slaveNode/slaveNode/settings.py
+++ slaveNode/slaveNode/settings.py
@@ -26,7 +26,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 0
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -55,6 +55,7 @@ DOWNLOADER_MIDDLEWARES = {
     # 'slaveNode.middlewares.MyCustomDownloaderMiddleware': 543,
     'slaveNode.rotate_useragent.RotateUserAgentMiddleware': 500,
     'slaveNode.wait_random_sec.WaitRandomSecMiddleware': 543,
+    'slaveNode.midproxy.ProxyMiddleware': 100,
     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
 }
 
@@ -107,6 +108,9 @@ REDIS_PORT = 6379
 FILTER_URL = None
 FILTER_HOST = '172.29.152.141'
 FILTER_PORT = 6379
+
+PROXY_PROVIDER_HOST = '172.29.152.141'
+PROXY_PROVIDER_PORT = 6379
 
 # Config about the Database
 import ssl
